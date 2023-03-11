69db1b4b13353cbce9d98adc266a42d28df0129f
Marisha Deroubaix <deroubaix.marisha@gmail.com>
2023-03-11 14:43:42 +0000

    fixes matches pages

--- routes/clicks.routes.js
+++ routes/clicks.routes.js
@@ -14,7 +14,7 @@ const { isAuthenticated } = require("../middleware/jwt.middleware");
   }
 }); */
 
-router.get("/clicks", isAuthenticated, async (req, res, next) => {
+/* router.get("/clicks", isAuthenticated, async (req, res, next) => {
   const { _id } = req.payload;
   try {
     const response = await User.find();
@@ -23,27 +23,57 @@ router.get("/clicks", isAuthenticated, async (req, res, next) => {
       (user) => user._id.toString() !== _id.toString()
     );
 
-    res.json(filteredResponse);
+    // res.json(filteredResponse);
 
     const match = filteredResponse.map((user) => {
+      const matchUsers = [];
       let count = 0;
       for (let i = 0; i < user.questionnaire.length; i++) {
         if (user.questionnaire[i]) {
           count++;
         }
         if (count >= 3) {
-          return true;
+          matchUsers.push(user);
         }
       }
-      return false;
-    });
+    }); 
 
+    res.json(match);
+    console.log(match);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});  */
+
+router.get("/clicks", isAuthenticated, async (req, res, next) => {
+  const { _id } = req.payload;
+  try {
+    const response = await User.find({ _id: { $ne: _id } }); // exclude current user from the search
+
+    const match = response
+      .map((user) => {
+        let count = 0;
+        for (let i = 0; i < user.questionnaire.length; i++) {
+          if (user.questionnaire[i]) {
+            count++;
+          }
+          if (count >= 3) {
+            return user;
+          }
+        }
+        return null;
+      })
+      .filter((user) => user !== null);
+
+    res.json(match);
     console.log(match);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong" });
   }
 });
+
 
 router.get("/clicks/:id", async (req, res, next) => {
   const { id } = req.params;
